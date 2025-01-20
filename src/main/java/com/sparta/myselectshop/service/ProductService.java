8719c6409c11c13d2478e6babee829e728bc2df0
Jong-Min <codejomo99@gmail.com>
2025-01-20 16:04:42 +0900

    feat: 상품 3Layer 구축

---
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -0,0 +1,14 @@
+package com.sparta.myselectshop.service;
+
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    public ProductResponseDto createProduct(ProductRequestDto productRequestDto) {
+        return null;
+    }
+}
