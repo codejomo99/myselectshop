686bf53ada4d58fe5a1b198d50cb0d276e02a868
Jong-Min <codejomo99@gmail.com>
2025-01-20 16:17:25 +0900

    feat: 상품저장 api

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -2,13 +2,21 @@ package com.sparta.myselectshop.service;
 
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    // 생성
     public ProductResponseDto createProduct(ProductRequestDto productRequestDto) {
-        return null;
+        Product product = productRepository.save(new Product(productRequestDto));
+
+        return new ProductResponseDto(product);
     }
 }
